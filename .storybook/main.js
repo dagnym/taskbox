3305d5ffd7c0aafa045bdebd9c486e14b8fe59a8
dagnym <50122744+dagnym@users.noreply.github.com>
2023-04-26 13:57:23 -0400

    added new TaskList component, added proptypes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,15 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  staticDirs: ["../public"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app",
-  ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-webpack5",
+  stories: ['../src/components/**/*.stories.js'],
+  staticDirs: ['../public'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app', '@storybook/addon-interactions', '@storybook/addon-a11y', '@storybook/addon-mdx-gfm'],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
   },
   features: {
-    interactionsDebugger: true,
+    interactionsDebugger: true
   },
+  docs: {
+    autodocs: true
+  }
 };
